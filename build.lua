e8541664a6caa22e02978b0a517b55765d6fe7dd
Floral Soda <lilysoda@pm.me>
2023-10-15 03:40:31 +0100

    Added modular installer

--- build.lua
+++ build.lua
@@ -1,33 +1,110 @@
 local root = "./PortOS"
-local file = fs.open("./PortOS/out.ete","w")
 
-local function scanFile(path, fil)
-	if path == "./PortOS/.git" or path == "./PortOS/LICENSE" or path == "./PortOS/README.md" or path == "./PortOS/.gitignore" or path == "./PortOS/build.lua" or path == "./PortOS/Users" or path == "./PortOS/out.ete" then
-		return
-	end
-	print("Discovered",path)
-	if fs.isDir(path) then
-		fil.write(path)
-		local list = fs.list(path)
-		if path == "./PortOS" then
-			fil.write("\""..(#list - 7).."\"D")
-		else
-			fil.write("\""..(#list).."\"D")
-		end
-		
-		for _,p in pairs(list) do
-			scanFile(path.."/"..p, fil)
-		end
-	else
-		local op = fs.open(path,"r")
-		local data = op.readAll()
-		op.close()
-		fil.write(path)
-		fil.write("\""..(#data).."\"F")
-		fil.write(data)
-	end
+local function loadConfigs()
+    local files = fs.list(root .. "/.build_configs")
+	
+	local configs = {}
+
+	local json_load_options = {
+		["parse_empty_array"] = false
+	}
+
+    for _, name in pairs(files) do
+		print("Discovered", name)
+        local stream = fs.open(root .. "/.build_configs/" .. name, "r")
+        local data = stream.readAll()
+        stream.close()
+        configs[name:sub(1,-6)] = textutils.unserialiseJSON(data, json_load_options)
+    end
+	
+	return configs
 end
 
+local function createPackage(name, config)
+    local package = {
+        ["preset"] = config.preset or false,
+        ["name"] = config.name,
+        ["description"] = config.description,
+        ["files"] = config.dependencies
+    }
+	table.insert(package.files, name)
+    local data = textutils.serialise(package)
+    local file = fs.open(root .. "/.out/update_packages/" .. name .. ".pkg", "w")
+    file.write(data)
+    file.close()
+	print("Produced package data")
+end
 
-scanFile(root, file)
+local function createETEInternal(path, dir, fil)
+    --Fill out directory data
+
+    local count = 0
+    if dir["dir"] then
+        count = count + #dir["dir"]
+    end
+    if dir["files"] then
+        count = count + #dir["files"]
+    end
+
+    fil.write(path)                   --Name
+    fil.write("\"" .. count .. "\"D") --Length and type
+
+	if dir["dir"] then
+ 	    for dpath, data in pairs(dir["dir"]) do
+            --Fill out subdirectories
+			local path = path .. "/" .. dpath
+			if fs.isDir(path) then
+	        	createETEInternal(path, data, fil)
+			else
+				print("Directory \"",path,"\" was not found")
+			end
+	    end
+	end
+
+	if dir["files"] then
+    	for _, fpath in pairs(dir["files"]) do
+            --Fill out files
+            local path = path .. "/" .. fpath
+			if fs.exists(path) then
+    	    	local op = fs.open(path, "r")
+	        	local data = op.readAll()
+	        	op.close()
+        		fil.write(path)
+        		fil.write("\"" .. (#data) .. "\"F")
+                fil.write(data)
+            else
+				print("Path \"",path,"\" was not found")
+			end
+        end
+	end
+end
+local function createETE(name, config)
+	local file = fs.open(root.."/.out/bin/"..name..".ete", "w")
+    createETEInternal(root, config.files, file)
+    file.close()
+	print("Produced ETE")
+end
+
+local pkgs = fs.list("PortOS/.out/update_packages")
+for _,p in pairs(pkgs) do
+	fs.delete("PortOS/.out/update_packages/"..p..".pkg")
+end
+local bins = fs.list("PortOS/.out/bin")
+for _,p in pairs(bins) do
+	fs.delete("PortOS/.out/bin"..p..".ete")
+end
+
+local configs = loadConfigs()
+local names = {}
+
+for name, data in pairs(configs) do
+	print("\n-= Packaging",name,"=-")
+    createPackage(name, data)
+    createETE(name, data)
+	
+	table.insert(names, name)
+end
+
+local file = fs.open("PortOS/.out/update_packages/list.dat", "w")
+file.write(textutils.serialise(names))
 file.close()
--- update.lua
+++ update.lua
@@ -1,11 +1,27 @@
 --The installer is currently found at https://pastebin.com/tKT0MVub
 
-local path = "https://raw.githubusercontent.com/FloralSoda/PortOS/main/out.ete"
-print("Downloading from github..")
+-- Confirm required functions exist
+if http == nil then
+    print("HTTP is not enabled. Please edit the config, or contact your server admin if you wish to install!")
+    return
+end
+
+local path = "https://raw.githubusercontent.com/FloralSoda/PortOS/main/.out/update_packages/list.dat"
+print("Downloading modules from github..")
 local request = http.get(path)
-local ete = request.readAll()
+local list = request.readAll()
 request.close()
 print("Data retrieved")
+
+local function recursivelyMakeDir(dir)
+    local cutoff = string.find(path, "/[^/]*$")
+    if cutoff ~= nil then
+        recursivelyMakeDir(dir:sub(1,cutoff))
+    elseif fs.isDir(cutoff) then
+		return
+    end
+	fs.makeDir(cutoff)
+end
 
 local function readETE(data)
 	local pathFlag = true
@@ -52,7 +68,9 @@ local function readETE(data)
 				if symbol == "F" then
 					typ = 1
 					charactersRead = 0
-					print("Unpacking ", path)
+                    print("Unpacking ", path)
+					local fileCutoff = string.find(path, "/[^/]*$")
+					recursivelyMakeDir(path:sub(1,fileCutoff))
 					file = fs.open(path, "w")
 				else
 					print("Creating directory at", path)
@@ -72,6 +90,205 @@ local function readETE(data)
 	end
 end
 
-print("Unpacking ETE file")
-readETE(ete)
-print("Successfully unpacked. Run PortOS/bin/boot.lua to execute")
+print("Getting package info")
+local pkg_list = textutils.unserialise(list)
+local pkgs = {}
+
+for _, pkg in pairs(pkg_list) do
+    local dlpath = "https://raw.githubusercontent.com/FloralSoda/PortOS/main/.out/update_packages/" .. pkg .. ".pkg"
+    print("Downloading info for:", pkg)
+    local request = http.get(dlpath)
+    local list = request.readAll()
+    request.close()
+    table.insert(pkgs, textutils.unserialise(list))
+end
+print("Successfully downloaded list")
+
+local function menu(items, startY)
+    local sidx = 1
+    local windowOffset = 0
+
+	local tx, ty = term.getSize()
+	ty = ty - (startY - 1)
+
+    local drawItem = function(item, number, selected, offset)
+        local y = (number - offset) + startY
+        if y < 1 or y > ty then
+            return --It's offscreen, no need to draw
+        end
+
+        term.setCursorPos(1, y)
+        if selected then
+            term.write("[ " .. item .. " ]")
+        else
+            term.write("  " .. item .. "  ")
+        end
+    end
+    local redraw = function()
+		term.clear()
+    	for i = 1, #items do
+        	drawItem(items[i], i, sidx == i, windowOffset)
+    	end
+	end
+
+	redraw()
+
+	while true do
+        local event, key = os.pullEvent("key")
+
+		if key == keys.up then
+            if sidx > 1 then
+                sidx = sidx - 1
+            end
+
+            if sidx == windowOffset then
+                windowOffset = windowOffset - 1
+                redraw()
+            end
+        elseif key == keys.down then
+            if sidx < #items then
+                sidx = sidx + 1
+            end
+
+            if sidx > windowOffset + ty then
+                windowOffset = windowOffset + 1
+                redraw()
+            end
+		elseif key == keys.pageUp then
+            windowOffset = math.max(windowOffset - ty, 0)
+            redraw()
+        elseif key == keys.pageDown then
+            windowOffset = math.min(windowOffset + ty, #items)
+			redraw()
+        elseif key == keys.enter then
+            return sidx, items[sidx]
+		end
+	end
+end
+
+local function installPackage(pkg)
+
+end
+
+local function compatMode()
+	local choosePackage = function(pkg)
+		term.clear()
+		term.setCursorPos(1,1)
+        print("Compatibility mode\n")
+        print("Package Details:")
+        print("Name:", pkg.name)
+        print("Requirements:")
+        for _, dep in pairs(pkg.files) do
+            print(" ", dep)
+        end
+        print("\n", pkg.description)
+        print("\nInstall?")
+
+        local _, y = term.getCursorPos()
+        local choice = menu({ "Yes, No" }, y)
+		
+		return choice == 1
+	end
+
+	local listPackages = function(set)
+        local names = {"Back"}
+        for _, pkg in pairs(set) do
+            table.insert(names, pkg.name)
+        end
+
+		while true do
+        	term.clear()
+			term.setCursorPos(1,1)
+        	print("Compatibility mode")
+
+			print("Select preset")
+		
+            local sidx = menu(names, 3)
+            if sidx == 1 then --User chose "Back"
+                return false
+            end
+			
+			local pkg = set[sidx - 1]
+			if choosePackage(pkg) then
+                installPackage(pkg)
+				return true
+			end
+		end
+	end
+	
+    local presets = function()
+		local presets = {}
+        for _, pkg in pairs(pkgs) do
+            if pkg.preset then
+                table.insert(presets, pkg)
+            end
+        end
+
+		return listPackages(presets)
+	end
+	local custom = function()
+        return listPackages(pkgs)
+	end
+	
+	while true do
+        term.clear()
+		term.setCursorPos(1,1)
+    	print("Compatibility mode")
+
+    	print("Select install")
+    	print("---------------------")
+    	local sidx = menu({
+        	"Presets",
+			"Custom"
+    	}, 3)
+
+		if sidx == 1 then
+            if presets() then
+                break
+            end
+        elseif sidx == 2 then
+			if custom() then
+				break
+			end
+		end
+	end
+end
+
+compatMode()
+
+-- print("Loading UI")
+-- local grayscaleConversion = {
+-- 	[colors.white] = colors.white,
+-- 	[colors.yellow] = colors.white,
+-- 	[colors.lime] = colors.white,
+-- 	[colors.lightBlue] = colors.lightGray,
+-- 	[colors.orange] = colors.lightGray,
+-- 	[colors.brown] = colors.gray,
+-- 	[colors.green] = colors.gray,
+-- 	[colors.red] = colors.gray,
+-- 	[colors.black] = colors.black,
+-- 	[colors.blue] = colors.gray,
+-- 	[colors.purple] = colors.gray,
+-- 	[colors.cyan] = colors.lightGray,
+-- 	[colors.lightGray] = colors.lightGray,
+-- 	[colors.gray] = colors.gray,
+-- 	[colors.pink] = colors.lightGray,
+-- 	[colors.magenta] = colors.gray,
+-- }
+-- local function toGrayscale(color)
+-- 	return grayscaleConversion[color]
+-- end
+-- local function drawPixel(x,y,color)
+-- 	if term.isColor() then
+--         term.setCursorPos(x, y)
+--         term.setBackgroundColor(color)
+--         term.write(" ")
+--     else
+--         term.setCursorPos(x, y)
+--         term.setBackgroundColor(toGrayscale(color))
+-- 		term.write(" ")
+-- 	end
+-- end
+-- local function drawBox(color)
+	
+-- end
